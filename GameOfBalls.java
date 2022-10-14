71e13cbeb995140ce745ea974f8c505b4d5ba071
Karn <90930089+karn05@users.noreply.github.com>
2022-10-14 19:18:05 +0700

    Add files via upload

---
+++ GameOfBalls.java
@@ -0,0 +1,12 @@
+package CS300_19_09;
+
+import java.util.*;
+
+public class GameOfBalls {
+ public static void main(String args[]) {
+	 int width, height, space, line;
+	 Scanner sc = new Scanner(System.in);
+	 
+	 
+ }
+}
