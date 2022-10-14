f38b5a4ff11e2b3061bef0fbe917139f903dcc53
Karn <90930089+karn05@users.noreply.github.com>
2022-10-14 19:38:33 +0700

    Update DatatypeExample.java

--- ชนิดข้อมูล/DatatypeExample.java
+++ ชนิดข้อมูล/DatatypeExample.java
@@ -1,14 +1,14 @@
 public class DatatypeExample {
 	 public static void main(String args[]) {
-     //integer
-     System.out.println("===== integer =====");
-		 int i = 9;
-     int thisNum = 99999;
-     System.out.println(thisNum); //output : 99999
-     System.out.println(thisNum+i); //output : 100008
+//integer
+	System.out.println("===== integer =====");
+		int i = 9;
+		int thisNum = 99999;
+		System.out.println(thisNum); //output : 99999
+		System.out.println(thisNum+i); //output : 100008
      
-     //float, double
-  		   System.out.println("===== float and double =====");
+//float, double
+  	System.out.println("===== float and double =====");
 		     double num1 = 10.11;
 		     double num2 = 11.12;
 		     float floater1 = 1.5f;
@@ -21,13 +21,22 @@ public class DatatypeExample {
 		     //output = Number num1 is 10.11 and num2 is 11.12 so num3 is 21.23 and for float it's 1.5000003
 		     System.out.printf(" and for float it's %f",floater1);
 		     System.out.printf("\n");
-
-     //String
-         System.out.println("===== String =====");
-         String first = "Sultans";
-         String second = "Away";
-         String space = " ";
-         String is = "is";
-         System.out.printf()
+//String
+	System.out.println("===== String =====");
+	         String sultan = "sultans";
+	         String swing = "swing";
+	         String space = " ";
+	         String of = "of";
+	         System.out.println(sultan.substring(0, 1).toUpperCase() + sultan.substring(1) 
+				    + space + of.substring(0, 1).toUpperCase() + of.substring(1, 2)
+				    + space + swing.substring(0, 1).toUpperCase() + swing.substring(1));
+		 //output = Sultans Of Swing
+		 
+//boolean
+	System.out.println("====== boolean ======");
+	boolean willSomedayEverCome = false;
+	boolean isThisTheAnswer = true;
+	System.out.println(willSomedayEverCome);
+	System.out.println(isThisTheAnswer);
 	 }
 }
